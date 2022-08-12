46ff7dc003040ccf6477ddaea6a98a894dfba4c3
Ichtyander <di_ignatov@mail.ru>
2022-08-12 21:39:21 +0300

    Добавьте файлы проекта.

---
+++ Task_1/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class BankAccount <T>
+    {
+        T accountNumber;
+        double accountBalance;
+        string accountOwner;
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите номер банковского счета");
+            accountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс банковского счета");
+            accountBalance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите имя владельца банковского счета");
+            accountOwner = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"Банковский счет номер {accountNumber}" + "\n" + $"Владелец счета: {accountOwner}" + "\n" + $"Баланс: {accountBalance}";
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1.    Создать класс для моделирования счета в банке. 
+ * Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  
+ * Класс должен быть объявлен как обобщенный. 
+ * Универсальный параметр T должен определять тип номера счета. 
+ * Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. 
+ * Создать  несколько экземпляров класса, параметризированного различными типам. 
+ * Заполнить его поля и вывести информацию об экземпляре класса на печать.*/
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тест 1
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.SetInfo();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            //Тест 2
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.SetInfo();
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
